6b599ee65394f4f7bc981dd5f70509f5fa65b9a8
arriee <arriecodes@gmail.com>
2020-08-28 13:49:57 +0300

    Finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
 console.log('JS file is loaded');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById('message-2');
+
+weatherForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const location = search.value;
+
+    messageOne.innerText = '';
+    messageTwo.innerText = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then((res) => {
+                res.json()
+                    .then ((data) => {
+                        if (data.error) {
+                            messageOne.innerText = data.error;
+                        } else {
+                            messageOne.innerText = data.location;
+                            messageTwo.innerText = data.forecast;
+                        }
+                    })
+            }
+        )
+
+})
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 
 const app = express();
 
@@ -39,7 +41,31 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    if (!req.query.address) {
+        return res.send({
+            error: 'Provide valid address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({error});
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error});
+            }
+
+            res.send({
+                location: location,
+                forecast: forecastData,
+                address: req.query.address
+            })
+        })
+
+
+    })
 })
 
 app.get('*', (req, res) => {
